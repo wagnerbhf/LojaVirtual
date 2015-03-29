91deb0366cf28da12571ce28337e0ec584f7a6cb
wagnerbhf@gmail.com <wagnerbhf@gmail.com>
2015-03-28 21:39:40 -0300

    email pedido

---
+++ Quiron.LojaVirtual.Dominio/Entidade/EmailConfiguracoes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+   public class EmailConfiguracoes
+   {
+
+      public bool UsarSsl = false;
+
+      public string ServidorSmtp = "smtp.quiron.com.br";
+
+      public int ServidorPorta = 587;
+
+      public string Usuario = "quiron";
+
+      public bool EscreverArquivo = false;
+
+      public string PastaArquivo = @"C:\envioemail";
+
+      public string De = "wagnerbhf@gmail.com";
+
+      public string Para = "wagnerbhf@gmail.com";
+   }
+}
