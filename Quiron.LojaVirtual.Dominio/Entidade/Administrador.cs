2b53fb2ec4acadf082601508b9c13b0c643d07c8
wagnerbhf@gmail.com <wagnerbhf@gmail.com>
2015-04-05 09:21:29 -0300

    novos fontes

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Administrador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+   public class Administrador
+   {
+      [Key]
+      public int Id { get; set; }
+
+      [Required(ErrorMessage = "Digite o login")]
+      [Display(Name = "Login:")]
+      public string Login { get; set; }
+
+      [Required(ErrorMessage = "Digite a senha")]
+      [Display(Name = "Senha:")]
+      [DataType(DataType.Password)]
+      public string Senha { get; set; }
+
+      public DateTime UltimoAcesso { get; set; }
+   }
+}
